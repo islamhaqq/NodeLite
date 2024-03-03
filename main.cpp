538bb3fc75580848d1a5e5d355169cb4394ce898
Syed Aman <syednashikaman@gmail.com>
2024-03-03 14:08:46 -0500

    Parse functions

--- main.cpp
+++ main.cpp
@@ -3,16 +3,46 @@
 #include <functional>
 #include <iostream>
 #include <string>
+#include <regex>
 
 std::vector<std::function<void()>> tasks;
 
-void interpretJS(const std::string& code) {
-    auto task = [code]() {
-        if (code == "console.log('Hello, World!');") {
-            std::cout << "Hello, World!" << std::endl;
+void interpretJS(std::string& code) {
+    // Regex to find function calls and definitions
+    std::regex funcDefRegex(R"(function\s+(\w+)\(\)\s*\{\s*console\.log\('(.+)'\);\s*\})");
+    std::regex funcCallRegex(R"((\w+)\(\);)");
+
+    std::smatch matches;
+    // Check for function definition
+    if (std::regex_search(code, matches, funcDefRegex)) {
+        if (matches.size() == 3) { // We expect 3 matches: the whole pattern, the function name, and the log message
+            std::string funcName = matches[1];
+            std::string message = matches[2];
+
+            // Create a task for the function definition
+            auto task = [funcName, message]() {
+                if (funcName == "displayMessage") { // We only support displayMessage for now
+                    std::cout << message << std::endl;
+                }
+            };
+            tasks.emplace_back(task);
         }
-    };
-    tasks.emplace_back(task);
+    }
+
+    // Check for function calls
+    while (std::regex_search(code, matches, funcCallRegex)) {
+        if (matches.size() > 1) {
+            std::string funcName = matches[1];
+
+            // If a function is called, add its task to the queue (if defined)
+            for (auto& task : tasks) {
+                // We need a way to identify tasks; for simplicity, we assume they're unique
+                // This is a limitation: in real scenarios, we'd need a better management of functions and scopes
+                task(); // Execute the task associated with the function call
+            }
+        }
+        code = matches.suffix().str(); // Continue searching from the end of the last match
+    }
 }
 
 void runEventLoop() {
---
+++ tests/displayMessage.js
@@ -0,0 +1,9 @@
+// Simple JavaScript file example
+
+// Function to display a message
+function displayMessage() {
+    console.log('Hello, World!');
+}
+
+// Call the function
+displayMessage();
