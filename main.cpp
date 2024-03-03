911c566f01799142cb8d15744e2cd7416038ffc8
Syed Aman <syednashikaman@gmail.com>
2024-03-03 17:15:53 -0500

    output stream to file

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <string>
 #include <regex>
 
-std::vector<std::function<void()>> tasks;
+std::vector<std::function<void(std::ostream&)>> tasks;
 
 void interpretJS(std::string& code) {
     std::regex consoleLogRegex(R"(console\.log\('(.+)'\);)");
@@ -13,46 +13,32 @@ void interpretJS(std::string& code) {
     std::regex functionCallRegex(R"((\w+)\(\);)");
 
     std::smatch matches;
-    // Check for function definition
     if (std::regex_search(code, matches, functionDefinitionRegex)) {
         if (matches.size() == 3) { // We expect 3 matches: the whole pattern, the function name, and the log message
             std::string funcName = matches[1];
             std::string message = matches[2];
 
-            // Create a task for the function definition
-            auto task = [funcName, message]() {
-                if (funcName == "displayMessage") { // We only support displayMessage for now
-                    std::cout << message << std::endl;
+            auto task = [funcName, message](std::ostream& out) {
+                if (funcName == "displayMessage") {
+                    out << message << std::endl;
                 }
             };
             tasks.emplace_back(task);
         }
     }
-
-    // Check for function calls
-    while (std::regex_search(code, matches, functionCallRegex)) {
-        if (matches.size() > 1) {
-            std::string funcName = matches[1];
-
-            for (auto& task : tasks) {
-                task();
-            }
-        }
-        code = matches.suffix().str(); // Continue searching from the end of the last match
-    }
 }
 
-void runEventLoop() {
+void runEventLoop(std::ostream& out) {
     while (!tasks.empty()) {
         auto task = tasks.front();
-        task();
+        task(out);
         tasks.erase(tasks.begin());
     }
 }
 
 int main(const int argc, char* argv[]) {
     if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <path-to-js-file>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <path-to-js-file> [output-file]" << std::endl;
         return 1;
     }
 
@@ -69,9 +55,22 @@ int main(const int argc, char* argv[]) {
     }
     file.close();
 
+    std::ofstream outFile;
+    std::ostream* outStream = &std::cout;
+
+    if (argc >= 3) {
+        outFile.open(argv[2]);
+        if (!outFile) {
+            std::cerr << "Unable to open file " << argv[2] << std::endl;
+            return 1;
+        } else {
+            outStream = &outFile;
+        }
+    }
+
     interpretJS(jsCode);
 
-    runEventLoop();
+    runEventLoop(*outStream);
 
     return 0;
 }
