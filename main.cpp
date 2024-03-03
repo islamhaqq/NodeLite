14ba6bc9e0b2de04c8ca581cef96c8c4be258a41
Syed Aman <syednashikaman@gmail.com>
2024-03-03 12:37:40 -0500

    simple event loop

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
+#include <functional>
 #include <iostream>
+#include <string>
+#include <unordered_map>
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+std::vector<std::function<void()>> tasks;
+
+void interpretJS(const std::string& code) {
+    auto task = [code]() {
+        if (code == "console.log('Hello, World!');") {
+            std::cout << "Hello, World!" << std::endl;
+        }
+    };
+    tasks.emplace_back(task);
+}
+
+void runEventLoop() {
+    while (!tasks.empty()) {
+        auto task = tasks.front();
+        task();
+        tasks.erase(tasks.begin());
+    }
+}
+
+int main() {
+    const std::string jsCode = "console.log('Hello, World!');";
+
+    interpretJS(jsCode);
+
+    runEventLoop();
+
     return 0;
 }
